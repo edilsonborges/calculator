3acbaa359362b0f6c2072487572795e0a1b18168
Edilson Borges <edilson.borges@agrodefesa.go.gov.br>
2020-07-21 22:11:34 -0300

    memory

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,11 +1,26 @@
+import 'package:calculator/models/memory.dart';
 import 'package:flutter/material.dart';
 import '../components/display.dart';
 import '../components/keyboard.dart';
 
-class Calculator extends StatelessWidget {
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+  _onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Column(children: <Widget>[Display('15315183'), Keyboard()]));
+        home: Column(
+            children: <Widget>[Display(memory.value), Keyboard(_onPressed)]));
   }
 }
